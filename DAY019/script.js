f88b06e7c69f22ab53ffc33557295c6ea4b2304c
hamuhamuowo <hemdezvous@gmail.com>
2024-11-24 14:24:00 +0900

    ♻️ refactoring script code!

--- DAY019/script.js
+++ DAY019/script.js
@@ -1,39 +1,26 @@
 document.querySelectorAll(".circle--color-empty").forEach((item) => {
   item.addEventListener("click", () => {
     console.log(item.id);
-    if (item.id === "circle1") {
-      const tl = gsap.timeline();
-      tl.to(".center", {
-        backgroundColor: "#b1d690",
-      }).to(
-        ".circle--color-fill",
-        {
-          x: 0,
-        },
-        "<"
-      );
-    } else if (item.id === "circle2") {
-      const tl = gsap.timeline();
-      tl.to(".center", {
-        backgroundColor: "#ffa24c",
-      }).to(
-        ".circle--color-fill",
-        {
-          x: 60,
-        },
-        "<"
-      );
-    } else {
-      const tl = gsap.timeline();
-      tl.to(".center", {
-        backgroundColor: "#ff77b7",
-      }).to(
-        ".circle--color-fill",
-        {
-          x: 120,
-        },
-        "<"
-      );
-    }
+    const backgroundColors = {
+      circle1: "#b1d690",
+      circle2: "#ffa24c",
+      circle3: "#ff77b7",
+    };
+    const xPositions = {
+      circle1: 0,
+      circle2: 60,
+      circle3: 120,
+    };
+
+    const tl = gsap.timeline();
+    tl.to(".center", {
+      backgroundColor: backgroundColors[item.id],
+    }).to(
+      ".circle--color-fill",
+      {
+        x: xPositions[item.id],
+      },
+      "<"
+    );
   });
 });
